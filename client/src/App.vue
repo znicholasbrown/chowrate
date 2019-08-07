56315930630e393073e818c7b7305bfb28b149b6
znicholasbrown <znicholasbrown@gmail.com>
2019-08-07 02:39:01 -0400

    Fixes some typescript module declaration issues.

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,18 +9,21 @@
 </template>
 
 <style lang="scss">
+html, body {
+  background-color: #333;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #DC7633;
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #DC7633;
     &.router-link-exact-active {
       color: #42b983;
     }
